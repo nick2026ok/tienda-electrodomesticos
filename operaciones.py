90aca672ae37cc77a21d947549d18cd62365367d
diego <programemos.juntosok@gmail.com>
2026-06-22 09:21:33 -0300

    se comienzan operaciones

---
+++ operaciones.py
@@ -0,0 +1,6 @@
+precio = 100    
+cantidad = 3
+total = precio * cantidad
+print(total)
+cantidad = 5
+print (total)  #se realiza la operación de multiplicación entre el precio y la cantidad para obtener el total
