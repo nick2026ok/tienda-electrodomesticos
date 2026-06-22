dafdaa7085aa316e35d58e29ed338e9608bdc754
diego <programemos.juntosok@gmail.com>
2026-06-22 09:24:08 -0300

    Se corrige y se agrega linea total= para que muestre nueva cantidad

--- operaciones.py
+++ operaciones.py
@@ -3,4 +3,5 @@ cantidad = 3
 total = precio * cantidad
 print(total)
 cantidad = 5
-print (total)  #se realiza la operación de multiplicación entre el precio y la cantidad para obtener el total
+total = precio * cantidad
+print(total)
