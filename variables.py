6b0455535e16d85cae49b099469c29a7110122da
diego <programemos.juntosok@gmail.com>
2026-06-22 09:04:15 -0300

    se escriben variables

---
+++ variables.py
@@ -0,0 +1,8 @@
+nombre = "Gabriel" #se indica el nombre de la variable, en este caso "nombre" y se asigna un valor a esa variable
+edad = 30
+ciudad = "Madrid"
+altura = 1.75
+print(nombre)   #se utiliza la función print para mostrar el valor de la variable "nombre" en la consola
+print(edad)
+print(ciudad)
+print(altura)
